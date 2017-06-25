eb68059ec342aa8f64517e85f5c083cb4eea410d
coderick14 <bhattacharyya.rick14@gmail.com>
2017-06-25 20:04:27 +0530

    Fix logging issues

--- experiment/views.py
+++ experiment/views.py
@@ -60,10 +60,10 @@ def initiation(req):
                         key = str(user_board.mid)
 			
                         settings.boards[key]["experiment_id"] = e.id
-                        global_logfile = settings.SBHS_GLOBAL_LOG_DIR + "/" + key + ".log"
-                        with open(global_logfile, "a") as global_loghandler:
-                            data = "\n\n===================New experiment====================\nUsername : " + user.username + "\nExperiment Id : " + str(e.id) + "\n"
-                            global_loghandler.write(data)
+                        # global_logfile = settings.SBHS_GLOBAL_LOG_DIR + "/" + key + ".log"
+                        # with open(global_logfile, "a") as global_loghandler:
+                        #     data = "\n\n===================New experiment====================\nUsername : " + user.username + "\nExperiment Id : " + str(e.id) + "\n"
+                        #     global_loghandler.write(data)
                             
                         reset(req)
 
@@ -206,12 +206,10 @@ def log_data(sbhs, mid, experiment_id, heat=None, fan=None, temp=None):
     if temp is None:
         temp = sbhs.getTemp()
 
-    data = "%f %s %s %s\n" % (time.time(), str(heat), str(fan), str(temp))
-    experiment_logfile = Experiment.objects.get(id=experiment_id).log
+    data = "%d %s %s %s\n" % (int(time.time()), str(heat), str(fan), str(temp))
     global_logfile = settings.SBHS_GLOBAL_LOG_DIR + "/" + str(mid) + ".log"
-    with open(global_logfile, "a") as global_loghandler, open(experiment_logfile, "a") as experiment_loghandler:
+    with open(global_logfile, "a") as global_loghandler:
         global_loghandler.write(data)
-        experiment_loghandler.write(data) 
 
 def validate_log_file(req):
     import hashlib
